3a2cc77ecb8ac4c84781d79e0da0c1ce56b891f0
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-06-25 18:14:47 -0700

    Add files via upload

--- Maximum Points You Can Obtain from Cards.py
+++ Maximum Points You Can Obtain from Cards.py
@@ -20,3 +20,10 @@ class Solution:
             s += cardPoints[i+M]
             mn = min(mn, s)
         return total - mn
+
+        # Solution 2
+        best = total = sum(cardPoints[:k])
+        for i in range (k-1, -1, -1):
+            total += cardPoints[i + len(cardPoints) - k] - cardPoints[i]
+            best = max(best, total)
+        return best
